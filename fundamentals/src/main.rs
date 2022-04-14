d681f8ec7ffcaf497f913cf536aab3ede9454481
Alexander Alexandrov <aalexandrov4@dxc.com>
2022-04-14 12:29:31 +0300

    Reorganize directories

---
+++ fundamentals/src/main.rs
@@ -0,0 +1,27 @@
+#![allow(dead_code)]
+#![allow(unused_must_use)]
+
+mod array_vector;
+mod enums;
+mod match_construct;
+mod tuples_structs;
+mod functions;
+mod generic_functions;
+mod boxing_unboxing;
+mod ownership;
+mod io;
+mod borrow_lifetimes;
+mod closures;
+mod strings;
+mod vector_update;
+
+use crate::ownership::ownership_example1;
+
+fn main() {
+    array_vector::_vectors();
+}
+
+// fn main() {
+//     vector_update::uniques();
+// }
+
